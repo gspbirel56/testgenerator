4c9951a27e74bc3c087e187f62eacb8f4eace05c
Chris Morris <gspbirel56@gmail.com>
2018-09-19 14:03:31 -0400

    Adding window customizations

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,8 +11,16 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Test Generator: Alpha");
+        primaryStage.setScene(new Scene(root, 500, 425));
+        //I want a fixed window size
+        primaryStage.setMaxHeight(425);
+        primaryStage.setMinHeight(425);
+        primaryStage.setMaxWidth(500);
+        primaryStage.setMinWidth(500);
+
+        //I also want to disable the maximize button
+
         primaryStage.show();
     }
 
